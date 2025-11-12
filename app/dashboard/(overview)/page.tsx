043379fccf4d88f9f72e0490b6af8197a58babb0
miraiangelo <leongelous@gmail.com>
2025-11-12 13:01:03 +0100

    feat: enhance dashboard and invoices pages with loading skeletons and card components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,8 +6,8 @@ import {
   fetchCardData,
 } from "@/app/lib/data";
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-
+import { LatestInvoicesSkeleton, RevenueChartSkeleton ,CardSkeleton} from '@/app/ui/skeletons';
+import CardWrapper from "@/app/ui/dashboard/cards";
 export default async function Page() {
   const {
     numberOfInvoices,
@@ -21,14 +21,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper/>
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
